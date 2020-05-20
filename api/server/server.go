04a107946e55357fafc04edfd97fa7b4b96b6625
Yacov Salomon <yacov.salomon@gmail.com>
2020-05-19 17:51:20 -0700

    fix: remove orlop-cassandra dependency

--- api/server/server.go
+++ api/server/server.go
@@ -3,7 +3,6 @@ package server
 import (
 	"context"
 	"github.com/switch-bit/orlop"
-	orlop_cassandra "github.com/switch-bit/orlop-cassandra"
 	"github.com/switch-bit/orlop/log"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -23,26 +22,19 @@ var (
 type Config struct {
 	Server    orlop.ServerConfig
 	Vault     orlop.VaultConfig
-	Cassandra orlop_cassandra.Config
 	TTL       time.Duration
 }
 
 // Server provides an implementation for the server
 type Server struct {
 	options   *Config
-	cassandra *orlop_cassandra.Connection
 }
 
 // NewServer returns a new server given the options
 func NewServer(ctx context.Context, opts *Config) (*Server, error) {
-	cassandra, err := orlop_cassandra.Connect(opts.Cassandra, opts.Vault)
-	if err != nil {
-		return nil, err
-	}
 
 	s := &Server{
 		options: opts,
-		cassandra: cassandra,
 	}
 	return s, nil
 }
@@ -76,15 +68,15 @@ func (s *Server) Authenticate(ctx context.Context, req interface{}, info *grpc.U
 		return nil, errNotAuthenticated
 	}
 
-	// Get the client ID (assuming one exists) for the API key, and error out if not found
-	var clientID string
-	err := s.cassandra.Query(ctx, `SELECT client_id FROM api_keys WHERE api_key = ? LIMIT 1`, apiKey).Scan(&clientID)
-	if err != nil {
-		return nil, errNotAuthorized
-	}
+	//// Get the client ID (assuming one exists) for the API key, and error out if not found
+	//var clientID string
+	//err := s.cassandra.Query(ctx, `SELECT client_id FROM api_keys WHERE api_key = ? LIMIT 1`, apiKey).Scan(&clientID)
+	//if err != nil {
+	//	return nil, errNotAuthorized
+	//}
 
-	// Add to the context
-	ctx = context.WithValue(ctx, clientIDContext, clientID)
+	//// Add to the context
+	//ctx = context.WithValue(ctx, clientIDContext, clientID)
 
 	return handler(ctx, req)
 }
